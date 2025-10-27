1c617656a497689e810b607a12831ed0f90ea59b
expressdeliveryservices <chibuikerich0@gmail.com>
2025-10-27 15:58:26 +0100

    Update Package.js
    class Package {     constructor(trackingNumber, sender, receiver, statusStages, location) {         this.trackingNumber = trackingNumber;         this.sender = sender;         this.receiver = receiver;         this.statusStages = statusStages;         this.currentStage = 0;         this.location = location;     } }

--- Package.js
+++ Package.js
@@ -1,16 +1,10 @@
-{
-  "name": "shipment-tracker-backend",
-  "version": "1.0.0",
-  "description": "Backend for Express Delivery Service",
-  "main": "node.js",
-  "scripts": {
-    "start": "node node.js"
-  },
-  "dependencies": {
-    "express": "^4.17.1",
-    "nodemailer": "^6.9.7",
-    "mongoose": "^6.0.0",
-    "cors": "^2.8.5",
-    "dotenv": "^16.0.0"
-  }
+class Package {
+    constructor(trackingNumber, sender, receiver, statusStages, location) {
+        this.trackingNumber = trackingNumber;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.statusStages = statusStages;
+        this.currentStage = 0;
+        this.location = location;
+    }
 }
